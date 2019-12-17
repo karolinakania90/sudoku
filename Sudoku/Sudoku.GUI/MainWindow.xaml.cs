c5434c83b95efccd79f11c9f1c395e460469e8e9
Kacper Gadula <k.gadula@interia.pl>
2019-12-17 21:36:27 +0100

    Name_Buttons

--- Sudoku/Sudoku.GUI/MainWindow.xaml.cs
+++ Sudoku/Sudoku.GUI/MainWindow.xaml.cs
@@ -24,5 +24,410 @@ namespace Sudoku.GUI
         {
             InitializeComponent();
         }
+
+        private void Button_0_0_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_1_0_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_2_0_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_3_0_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_4_0_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_5_0_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_6_0_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_7_0_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_8_0_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_0_1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_1_1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_2_1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_3_1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_4_1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_5_1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_6_1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_7_1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_8_1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_0_2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_1_2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_2_2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_3_2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_4_2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_5_2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_6_2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_7_2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_8_2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_0_3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_1_3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_2_3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_3_3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_4_3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_5_3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_6_3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_7_3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_8_3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_0_4_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_1_4_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_2_4_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_3_4_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_4_4_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_5_4_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_6_4_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_7_4_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_8_4_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_0_5_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_1_5_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_2_5_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_3_5_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_4_5_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_5_5_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_6_5_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_7_5_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_8_5_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_0_6_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_1_6_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_2_6_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_3_6_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_4_6_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_5_6_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_6_6_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_7_6_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_8_6_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_0_7_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_1_7_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_2_7_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_3_7_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_4_7_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_5_7_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_6_7_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_7_7_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_8_7_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_0_8_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_1_8_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_2_8_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_3_8_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_4_8_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_5_8_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_6_8_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_7_8_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_8_8_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
